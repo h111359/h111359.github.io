f04fa517f7b512f33ccf76b50ef2cd4305669200
Hristo Hristov <hmhristov@outlook.com>
2025-09-22 23:36:25 +0300

    Add silk, tabs on left

---
+++ 202507_china/data/event-20250719-flight.js
@@ -0,0 +1,18 @@
+window.GALLERY_ITEMS = [
+  { name: "20250719_205554.jpg", preview: "https://drive.google.com/file/d/1PH0P0S9V4By3NsH__J52Fg9tfIo2OARm/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_000843.jpg", preview: "https://drive.google.com/file/d/1tkHNHilw0pFO0cf7ZKXR6Ihwf84N4pME/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_013053.jpg", preview: "https://drive.google.com/file/d/16MChjVfYy31yu-fWtDSxsxhix4V7w0Ak/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_050326.jpg", preview: "https://drive.google.com/file/d/1WyGTWWzyB-eGatRRbHsBz9UYPRtY-Hu1/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_050344.jpg", preview: "https://drive.google.com/file/d/1yxWwzG5_lykASbpJpQWP0ebvrsKns4Hz/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_052713.jpg", preview: "https://drive.google.com/file/d/1Lg4YrxlUTSKwAc6mDt_M1oqIv-uWK9D3/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_140423.jpg", preview: "https://drive.google.com/file/d/1DSid8LlVLDtjo_FY7DgIkCMSiDyVyxQY/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_140500.jpg", preview: "https://drive.google.com/file/d/1z5arxVNiJ-gatp1LvR-lDE3N4XyfBS-F/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_215858.jpg", preview: "https://drive.google.com/file/d/1VWfrpRkHbSRWImgaSRpPd0AEHcMdlX2X/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_224959.jpg", preview: "https://drive.google.com/file/d/1N_nXNCh4uLUQhCauLieYCpP5bn713ffI/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_225001.jpg", preview: "https://drive.google.com/file/d/1h5UvMZ9LfjB9wj7msqdZTnn7lxDuXqIm/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_225211.jpg", preview: "https://drive.google.com/file/d/1EV5dgMYPWn5dJEAiq7PndaPDnWy-4ehy/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_225238.jpg", preview: "https://drive.google.com/file/d/1x6275pAcSyz19UlhKlbS1hR_J_50We3x/preview?authuser=0", desc: "Снимка – временно описание." },
+  { name: "20250720_225242.jpg", preview: "https://drive.google.com/file/d/1i9p97AlrCIaKne2sdTiaDHR3VPpDDgJ6/preview?authuser=0", desc: "Снимка – временно описание." }
+];
+
+
--- 202507_china/data/events.js
+++ 202507_china/data/events.js
@@ -1,7 +1,10 @@
 // /202507_china/data/events.js
 window.GALLERY_EVENT_INDEX = [
-  { slug: "20250722-04-acrobat", title: "20250722_04 Акробатично шоу", data: "data/event-20250722-04-acrobat.js" }
-  { slug: "20250723-ichan", title: "Ичан · 23 юли 2025", data: "data/event-20250723-ichan.js" },
-  { slug: "20250724-tudja", title: "20250724_08 Племето Туджа от народа Ба", data: "data/event-20250724-tudja.js" },
+  { slug: "event-20250719-flight", title: "20250719_Китай_полет_Шанхай", data: "data/event-20250719-flight.js" },
+  { slug: "event-20250721-nefrit-buda", title: "20250721_01_Шанхай_храм_Нефритеният_Буда", data: "data/event-20250721-nefrit-buda.js" },
+  { slug: "event-20250721-02-koprina", title: "20250721_02 Шанхай фабрика за коприна", data: "data/event-20250721-02-koprina.js" },  
+  { slug: "20250722-04-acrobat", title: "20250722_04 Акробатично шоу", data: "data/event-20250722-04-acrobat.js" },
+  { slug: "20250723-ichan", title: "20250723 Пътуване до Ичан", data: "data/event-20250723-ichan.js" },
+  { slug: "20250724-tudja", title: "20250724_08 Племето Туджа от народа Ба", data: "data/event-20250724-tudja.js" }
   
 ];
