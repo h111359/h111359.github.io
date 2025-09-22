de07ac96675c0d90efd407c88559c65c3c974197
Hristo Hristov <hmhristov@outlook.com>
2025-09-22 22:48:03 +0300

    Fix a list

--- 202507_china/data/events.js
+++ 202507_china/data/events.js
@@ -1,6 +1,6 @@
 // /202507_china/data/events.js
 window.GALLERY_EVENT_INDEX = [
-  { slug: "20250723-ichan", title: "Ичан · 23 юли 2025", data: "data/event-20250723-ichan.js" }
-  { slug: "20250724-tudja", title: "20250724_08 Племето Туджа от народа Ба", data: "data/event-20250724-tudja.js" }
+  { slug: "20250723-ichan", title: "Ичан · 23 юли 2025", data: "data/event-20250723-ichan.js" },
+  { slug: "20250724-tudja", title: "20250724_08 Племето Туджа от народа Ба", data: "data/event-20250724-tudja.js" },
   { slug: "20250722-04-acrobat", title: "20250722_04 Акробатично шоу", data: "data/event-20250722-04-acrobat.js" }
 ];
