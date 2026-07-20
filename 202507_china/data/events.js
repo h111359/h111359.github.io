43699d8ef1a2c42f6ae369d3f58f5285589c3ee3
Hromar <h111359@gmail.com>
2026-07-20 07:24:09 +0300

    fix double entry in the events list

--- 202507_china/data/events.js
+++ 202507_china/data/events.js
@@ -1,23 +1,20 @@
-// /202507_china/data/events.js
 window.GALLERY_EVENT_INDEX = [
   { slug: "event-20250719-flight", title: "20250719_Китай_полет_Шанхай", data: "data/event-20250719-flight.js" },
-
   { slug: "event-20250721-nefrit-buda", title: "20250721_01 Шанхай храм Нефритения Буда", data: "data/event-20250721-nefrit-buda.js" },
-  { slug: "event-20250721-02-koprina", title: "20250721_02 Шанхай фабрика за коприна", data: "data/event-20250721-02-koprina.js" },  
-  { slug: "event-20250721_03-frenska-kontsesia", title: "20250721_03 Шанхай френската концесия", data: "data/event-20250721_03-frenska-kontsesia.js"},
-  { slug: "event-20250721_04-razhodka-shanghai", title: "20250721_04 Разходка в Шанхай", data: "data/event-20250721_04-razhodka-shanghai.js"},
-  { slug: "event-20250721_05-lui-vuiton", title: "20250721_05 Шанхай - инсталация на Луи Вюитон", data: "data/event-20250721_05-lui-vuiton.js"},
-  { slug: "event-20250721_06-huanpu", title: "20250721_06 Шанхай - разходка с корабче по река Хуанпу", data: "data/event-20250721_06-huanpu.js"},
-  { slug: "event-20250722_01-tower", title: "20250722_01 Шанхайската кула", data: "data/event-20250722_01-tower.js"},
-  { slug: "event-20250722_02-uan", title: "20250722_02 Шанхай градините Ю Юан", data: "data/event-20250722_02-uan.js"},
-  { slug: "event-20250722_03-djudjadjao", title: "20250722_03 Джуджаджао", data: "data/event-20250722_03-djudjadjao.js"},
+  { slug: "event-20250721-02-koprina", title: "20250721_02 Шанхай фабрика за коприна", data: "data/event-20250721-02-koprina.js" },
+  { slug: "event-20250721_03-frenska-kontsesia", title: "20250721_03 Шанхай френската концесия", data: "data/event-20250721_03-frenska-kontsesia.js" },
+  { slug: "event-20250721_04-razhodka-shanghai", title: "20250721_04 Разходка в Шанхай", data: "data/event-20250721_04-razhodka-shanghai.js" },
+  { slug: "event-20250721_05-lui-vuiton", title: "20250721_05 Шанхай - инсталация на Луи Вюитон", data: "data/event-20250721_05-lui-vuiton.js" },
+  { slug: "event-20250721_06-huanpu", title: "20250721_06 Шанхай - разходка с корабче по река Хуанпу", data: "data/event-20250721_06-huanpu.js" },
+  { slug: "event-20250722_01-tower", title: "20250722_01 Шанхайската кула", data: "data/event-20250722_01-tower.js" },
+  { slug: "event-20250722_02-uan", title: "20250722_02 Шанхай градините Ю Юан", data: "data/event-20250722_02-uan.js" },
+  { slug: "event-20250722_03-djudjadjao", title: "20250722_03 Джуджаджао", data: "data/event-20250722_03-djudjadjao.js" },
   { slug: "event-20250722-04-acrobat", title: "20250722_04 Акробатично шоу", data: "data/event-20250722-04-acrobat.js" },
   { slug: "event-20250723-ichan", title: "20250723 Пътуване до Ичан", data: "data/event-20250723-ichan.js" },
   { slug: "event-20250724-tudja", title: "20250724_08 Племето Туджа от народа Ба", data: "data/event-20250724-tudja.js" },
-  { slug: "event-20250724-tudja", title: "20250724_08 Племето Туджа от народа Ба", data: "data/event-20250724-tudja.js" },
   { slug: "event-20250724_14-damn", title: "20250724_14 Язовир Трите клисури", data: "data/event-20250724_14-damn.js" },
-  { slug: "event-20250724_21-ship-evening", title: "20250724_21 Вечер на кораба", data: "data/event-20250724_21-ship-evening.js" },  
-  { slug: "event-20250725_08-ship-trip", title: "20250725_08 Яндзъ разходка с корабче", data: "data/event-20250725_08-ship-trip.js" }, 
+  { slug: "event-20250724_21-ship-evening", title: "20250724_21 Вечер на кораба", data: "data/event-20250724_21-ship-evening.js" },
+  { slug: "event-20250725_08-ship-trip", title: "20250725_08 Яндзъ разходка с корабче", data: "data/event-20250725_08-ship-trip.js" },
   { slug: "event-20250725_10-ship-walk", title: "20250725_10 Из корабчето", data: "data/event-20250725_10-ship-walk.js" },
   { slug: "event-20250725_13-yandzi", title: "20250725_11 Яндзъ", data: "data/event-20250725_13-yandzi.js" },
   { slug: "event-20250725_13-yandzi-gorge", title: "20250725_13 Яндзъ през клисурата", data: "data/event-20250725_13-yandzi-gorge.js" },
@@ -48,8 +45,4 @@ window.GALLERY_EVENT_INDEX = [
   { slug: "event-20250731_13-skytemple", title: "20250731_13 Храма на небето", data: "data/event-20250731_13-skytemple.js" },
   { slug: "event-20250731_17-watershow", title: "20250731_17 Водно шоу", data: "data/event-20250731_17-watershow.js" },
   { slug: "event-20250731_18-pekinduck", title: "20250731_18 Патица по пекински", data: "data/event-20250731_18-pekinduck.js" }
- 
-  // { slug: "", title: "", data: "" },  
-  
-
 ];
