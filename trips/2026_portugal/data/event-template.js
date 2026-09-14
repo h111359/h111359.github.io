0dad681678d13f42cb36d649528e90de0432f549
Hromar <h111359@gmail.com>
2026-09-14 20:53:36 +0300

    portugal - Belem tower

---
+++ trips/2026_portugal/data/event-template.js
@@ -0,0 +1,6 @@
+/**
+ * event-template.js: Optional empty event-data starting point for a new trip.
+ * Copy or generate a new event file, then register it through trips/data_editor.html.
+ */
+
+window.GALLERY_ITEMS = [];
---
+++ trips/2026_portugal/data/events.js
@@ -0,0 +1,7 @@
+window.GALLERY_EVENT_INDEX = [
+  {
+    "slug": "event-20260906-kulata-belem",
+    "title": "\u041a\u0443\u043b\u0430\u0442\u0430 \u0411\u0435\u043b\u0435\u043c",
+    "data": "data/event-20260906-kulata-belem.js"
+  }
+];
